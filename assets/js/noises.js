cc3ed0a5b36739b94827ff330f51a01fad5b034c
steelmaker86 <Steelmaker86@gmail.com>
2021-05-11 07:57:20 -0500

    Update noises.js

--- assets/js/noises.js
+++ assets/js/noises.js
@@ -1,29 +1,19 @@
 const soundsElement = document.querySelector('.sound-container');
-
 (async () => {
-    const sounds = await loadSounds();
-    addSoundsToPage(sounds)
+  const sounds = await loadSounds();
+  addSoundsToPage(sounds)
 })();
-
 async function loadSounds() {
-    const response = await fetch("sounds.json")
-    const json = await response.json()
-    return json;
+  const response = await fetch("sounds.json") const json = await response.json() return json;
 }
 
 function addSoundsToPage(sounds) {
-    sounds.forEach(sound => {
-        const soundButton = document.createElement("button")
-        soundButton.className = "btn btn-info btn-round btn-foob"
-        soundButton.textContent = sound.title;
-        
-        const player = document.createElement("audio")
-        soundsElement.appendChild(player)
-        player.setAttribute("src", `assets/audio/${sound.src}`)
-        
-        soundButton.addEventListener('click', () => {
-            player.play();
-        });
-        soundsElement.appendChild(soundButton)    
+  sounds.forEach(sound => {
+    const soundButton = document.createElement("button") soundButton.className = "btn btn-info btn-round btn-foob"
+    soundButton.textContent = sound.title;
+    const player = document.createElement("audio") soundsElement.appendChild(player) player.setAttribute("src", `assets/audio/${sound.src}`) soundButton.addEventListener('click', () => {
+      player.play();
     });
+    soundsElement.appendChild(soundButton)
+  });
 }
