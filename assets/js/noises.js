bdcc34ea5f2935a8b4893a6e3e78b7d1d175bc52
steelmaker86 <Steelmaker86@gmail.com>
2021-05-11 08:00:24 -0500

    Update noises.js

--- assets/js/noises.js
+++ assets/js/noises.js
@@ -4,14 +4,18 @@ const soundsElement = document.querySelector('.sound-container');
   addSoundsToPage(sounds)
 })();
 async function loadSounds() {
-  const response = await fetch("sounds.json") const json = await response.json() return json;
+  const response = await fetch("sounds.json")
+  const json = await response.json() return json;
 }
 
 function addSoundsToPage(sounds) {
   sounds.forEach(sound => {
     const soundButton = document.createElement("button") soundButton.className = "btn btn-info btn-round btn-foob"
     soundButton.textContent = sound.title;
-    const player = document.createElement("audio") soundsElement.appendChild(player) player.setAttribute("src", `assets/audio/${sound.src}`) soundButton.addEventListener('click', () => {
+    const player = document.createElement("audio") soundsElement.appendChild(player) 
+    player.setAttribute("src", `assets/audio/${sound.src}`)
+    
+    soundButton.addEventListener('click', () => {
       player.play();
     });
     soundsElement.appendChild(soundButton)
