7bccd47519e8ccae65e86a061497098efe994418
steelmaker86 <Steelmaker86@gmail.com>
2021-05-11 07:50:36 -0500

    Add files via upload

---
+++ assets/js/noises.js
@@ -0,0 +1,29 @@
+const soundsElement = document.querySelector('#sound-container');
+
+(async () => {
+    const sounds = await loadSounds();
+    addSoundsToPage(sounds)
+})();
+
+async function loadSounds() {
+    const response = await fetch("sounds.json")
+    const json = await response.json()
+    return json;
+}
+
+function addSoundsToPage(sounds) {
+    sounds.forEach(sound => {
+        const soundButton = document.createElement("button")
+        soundButton.className = "btn btn-info btn-round btn-foob"
+        soundButton.textContent = sound.title;
+        
+        const player = document.createElement("audio")
+        soundsElement.appendChild(player)
+        player.setAttribute("src", `assets/audio/${sound.src}`)
+        
+        soundButton.addEventListener('click', () => {
+            player.play();
+        });
+        soundsElement.appendChild(soundButton)    
+    });
+}
