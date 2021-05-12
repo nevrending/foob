3bcde439ba3384ffcd4cba26da3d8566abae2c23
bempfoobq052021 <83710956+bempfoobq052021@users.noreply.github.com>
2021-05-12 18:31:14 +0700

    Sound Button : Scripts, Styling, and Placement
    sounds.json got updated.
    noises.html now shows every voices in flex box, more responsive.. but it kind of looks unpleasant with the last buttons going full width.
    noises.js updates to now querySelect sound-buttons. I need to add some reasons as to why I do that. Also, removing btn-round and append audio element to sound button.
    custom.css got new class called sound button, sound-container-col, sound-container-row.

--- assets/js/noises.js
+++ assets/js/noises.js
@@ -1,26 +1,39 @@
-const soundsElement = document.querySelector('.sound-container');
+const soundsElement = document.querySelector('.sound-buttons');
 (async () => {
    const sounds = await loadSounds();
    addSoundsToPage(sounds)
 })();
+
+/**
+ *  @returns {Object}
+*/
+
 async function loadSounds() {
    const response = await fetch("sounds.json")
    const json = await response.json()
    return json;
 }
 
+/**
+ *  @param {Object} sounds - JSON-type list of fbk noises.
+ *  @param {string} sounds.title - FBK noise title.
+ *  @param {string} sounds.src - FBK noise URL.
+ */
+
 function addSoundsToPage(sounds) {
    sounds.forEach(sound => {
       const soundButton = document.createElement("button")
-      soundButton.className = "btn btn-info btn-round sound-button"
+      soundButton.className = "col btn btn-info sound-button"
       soundButton.textContent = sound.title;
-      const player = document.createElement("audio")
-      soundsElement.appendChild(player)
+      soundsElement.appendChild(soundButton) //the button is 
+	  //now the first and the last element in the list
+      
+	  const player = document.createElement("audio")
+      soundButton.appendChild(player)  //audio inside button.
+	  
       player.setAttribute("src", `assets/audio/${sound.src}`)
-
       soundButton.addEventListener('click', () => {
          player.play();
       });
-      soundsElement.appendChild(soundButton)
    });
 }
