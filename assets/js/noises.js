fc69f93dc1b5041c7ff8d89c0fd90baaf34c5e93
steelmaker86 <Steelmaker86@gmail.com>
2021-05-13 10:40:10 -0500

    Added changes to my own branch
    Need to catch up and test

--- assets/js/noises.js
+++ assets/js/noises.js
@@ -1,26 +1,32 @@
-const soundsElement = document.querySelector('.sound-container');
+const soundsElement = $('#sound-buttons');
 (async () => {
-   const sounds = await loadSounds();
-   addSoundsToPage(sounds)
+    const sounds = await loadSounds();
+    addSoundsToPage(sounds)
 })();
+
+/**
+ *  @returns {Object}
+*/
 async function loadSounds() {
-   const response = await fetch("sounds.json")
-   const json = await response.json()
-   return json;
+    const response = await fetch("assets/etc/sounds.json")
+    const json = await response.json()
+    return json;
 }
 
+/**
+ *  @param {Object} sounds - JSON-type list of fbk noises.
+ *  @param {string} sounds.title - FBK noise title.
+ *  @param {string} sounds.src - FBK noise URL.
+ */
 function addSoundsToPage(sounds) {
-   sounds.forEach(sound => {
-      const soundButton = document.createElement("button")
-      soundButton.className = "btn btn-info btn-round sound-button"
-      soundButton.textContent = sound.title;
-      const player = document.createElement("audio")
-      soundsElement.appendChild(player)
-      player.setAttribute("src", `assets/aud/fbk/noises/${sound.src}`)
+    sounds.forEach(sound => {
+        const soundButton = $(`<button class="col btn btn-info sound-button">${sound.title}</button>`)
+        soundsElement.append(soundButton) //the button is now the first and the last element in the list
+        const player = $(`<audio src="assets/aud/fbk/noises/${sound.src}">`)
+		soundButton.append(player)  //audio inside button.
+        soundButton.addEventListener('click', () => {
+            player.play();
+        });
 
-      soundButton.addEventListener('click', () => {
-         player.play();
-      });
-      soundsElement.appendChild(soundButton)
-   });
+    });
 }
