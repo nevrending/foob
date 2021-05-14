a214fcb2b1f13ef50f56e2e22f15e214184b03b7
Yefta Sutanto <yeftasutanto@gmail.com>
2021-05-14 17:56:27 +1000

    Noises.html : Improving on Riki's idea. (Superseding #5) (#6)
    * Create noises.html
    
    * Update foob.html
    
    * Update index.html
    
    * Update index.html
    
    * Update noises.html
    
    * Update noises.html
    
    * Update noises.html
    
    * added placeholder background picture for noises
    
    * Update noises.html
    
    * Delete placeholder.png
    
    * Add files via upload
    
    * Add files via upload
    
    * Update sounds.json
    
    * Update noises.js
    
    * Update noises.js
    
    * Update noises.js
    
    * Update noises.js
    
    * Update noises.js
    
    * Update noises.js
    
    * Update custom.css
    
    * Update noises.js
    
    * Update custom.css
    
    * Update noises.html
    
    Moving noises.js to <head>
    
    * Sound Button : Scripts, Styling, and Placement
    
    sounds.json got updated.
    noises.html now shows every voices in flex box, more responsive.. but it kind of looks unpleasant with the last buttons going full width.
    noises.js updates to now querySelect sound-buttons. I need to add some reasons as to why I do that. Also, removing btn-round and append audio element to sound button.
    custom.css got new class called sound button, sound-container-col, sound-container-row.
    
    * Update custom.css
    
    * Changes in main backgroudn image
    
    Custom.css
    <video> min width, width, and left align
    .presentation-title adjust for responsive mobile
    
    index.html
    changes inline style to css based style.
    
    * Footer copyright alignment
    
    Custom.css changes in flex align-items: center etc..
    index.html changes in <span class="copyright"> to <p class=....>
    
    * adjust footer; add Nyan as contrib; re-order and label CSS sections
    
    * Update on noises : Files, linkages, and lists
    
    Sync with new audio files from the trunk.
    Deleting old audio folder.
    Updating path on noises.js
    updating noise list in sounds.json
    
    * Adjusting "Shirakami Fubuki" title. (#1)
    
    * Update custom.css
    
    * Update custom.css
    
    Additional background to custom class .presentation-title to control the color, -webkit-bg-clip to text, -webkit-txt-fc to make the text transparent
    
    * Update index.html
    
    Cosmetic changes.
    Changing from <h1> tag to <span> tag because h1 functions like <p> and handles color gradient per whole sentence rather than per word. <span> handles color gradient per word.
    
    * Update index.html
    
    * Changes in main backgroudn image
    
    Custom.css
    <video> min width, width, and left align
    .presentation-title adjust for responsive mobile
    
    index.html
    changes inline style to css based style.
    
    * Update index.html
    
    <span> : </h1> to </span>
    
    Co-authored-by: Yefta Sutanto <yeftasutanto@gmail.com>
    
    * Update assets/css/custom.css
    
    space changes.
    
    Co-authored-by: Yefta Sutanto <yeftasutanto@gmail.com>
    
    * Update assets/css/custom.css
    
    space changes.
    
    Co-authored-by: Yefta Sutanto <yeftasutanto@gmail.com>
    
    * Update assets/css/custom.css
    
    space changes.
    
    Co-authored-by: Yefta Sutanto <yeftasutanto@gmail.com>
    
    * Footer copyright alignment
    
    Custom.css changes in flex align-items: center etc..
    index.html changes in <span class="copyright"> to <p class=....>
    
    Co-authored-by: Yefta Sutanto <yeftasutanto@gmail.com>
    
    * adjust footer; add Nyan as contrib; re-order and label CSS sections
    
    * cleanups
    
    * add Riki as contributor
    
    Co-authored-by: steelmaker86 <Steelmaker86@gmail.com>
    Co-authored-by: bempfoobq052021 <83710956+bempfoobq052021@users.noreply.github.com>

---
+++ assets/js/noises.js
@@ -0,0 +1,36 @@
+const soundsElement = document.querySelector('.sound-buttons');
+(async () => {
+    const sounds = await loadSounds();
+    addSoundsToPage(sounds)
+})();
+
+/**
+ *  @returns {Object}
+*/
+async function loadSounds() {
+    const response = await fetch("assets/etc/sounds.json")
+    const json = await response.json()
+    return json;
+}
+
+/**
+ *  @param {Object} sounds - JSON-type list of fbk noises.
+ *  @param {string} sounds.title - FBK noise title.
+ *  @param {string} sounds.src - FBK noise URL.
+ */
+function addSoundsToPage(sounds) {
+    sounds.forEach(sound => {
+        const soundButton = document.createElement("button")
+        soundButton.className = "col btn btn-info sound-button"
+        soundButton.textContent = sound.title;
+        soundsElement.appendChild(soundButton) //the button is now the first and the last element in the list
+
+        const player = document.createElement("audio")
+        soundButton.appendChild(player)  //audio inside button.
+
+        player.setAttribute("src", `assets/aud/fbk/noises/${sound.src}`)
+        soundButton.addEventListener('click', () => {
+            player.play();
+        });
+    });
+}
