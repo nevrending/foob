0355eff456057ca2ef465d5e0f16e20a58d42f58
steelmaker86 <Steelmaker86@gmail.com>
2021-05-30 17:19:24 -0500

    Update mainpage.js

--- assets/js/mainpage.js
+++ assets/js/mainpage.js
@@ -4,6 +4,6 @@ let imgI = 1
 function rotateImage() {
     ++imgI
     if (imgI > 3) imgI = 1
-    yosettiImage.src = `assets/img/pg${imgI}.png`
+    yosettiImage.src = `assets/img/fbk/pg${imgI}.png`
 }
 setInterval(rotateImage(), 10000);
